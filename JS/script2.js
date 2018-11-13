a9fdc3b54f413ee35d06c4e1e1acf53d0acc44a3
TKaferi <tkaferi@gmail.com>
2018-11-13 22:31:54 +0200

    added  script2

---
+++ JS/script2.js
@@ -0,0 +1,12 @@
+//факториал числа n.
+
+var n = 5;
+var res = 1;
+if (n < 0) {
+    console.log('ошибка: ' + n + ' отрицательное число');
+} else {
+    for (var i = 1; i <= n; i++) {
+        res *= i;  
+    }
+    console.log(n + '! = ' + res);
+}
