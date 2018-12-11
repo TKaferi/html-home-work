5384d9324dd371805efdc39592f82dce94e01e74
TKaferi <tkaferi@gmail.com>
2018-12-11 19:59:20 +0200

    fixed file tabs.js

--- JS/tabs/tabs.js
+++ JS/tabs/tabs.js
@@ -7,22 +7,15 @@ document.querySelector('.tabNav').onclick = function (e) {
   var target = e.target;
   for (var i = 0; i < navItem.length; i++) {
     itemTab[i].classList.remove('show');
+    navItem[i].classList.remove('active');
     if (target === navItem[i]) {
       showTab(i);
     }
   }
-  for (var i = 0; i < itemTab.length; i++) {
-    navItem[i].classList.remove('active');
-    if (target === navItem[i]) {
-      activTab(i);
-    }
-  }
-
 }
 
 function showTab(i) {
   itemTab[i].classList.add('show');
-}
-function activTab(i) {
   navItem[i].classList.add('active');
 }
+
