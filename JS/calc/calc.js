634d2b2b4304629338a17673f66fba11effbd509
TKaferi <tkaferi@gmail.com>
2018-12-11 21:42:40 +0200

    example fetch

--- JS/calc/calc.js
+++ JS/calc/calc.js
@@ -4,3 +4,29 @@ function plus() {
     document.forms[0]['result'].value = first + second;
 }
 
+/*
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'phones.json', false);
+xhr.send();
+
+//xhr.onload = function() {
+    if (xhr.status != 200) {
+        console.log(xhr.status + ': ' + xhr.statusText);
+    } else {
+        console.log(xhr.responseText);
+        var obj = JSON.parse(xhr.responseText);
+        for (var i = 0; i < obj.length; i++) {
+            document.write(obj[i].age + '<br />');
+        }
+    }
+//}
+*/
+var promise	= fetch('phones.json');
+promise.then(function(response) {
+    //var obj = response.json();
+    return response.json();
+})
+.then(function(data) {
+    alert(data[0].id);
+});
+
